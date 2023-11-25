724ab7662681d2c1d56a380dbccada220449710c
goztrk <me@gokhan.org.tr>
2023-11-25 16:40:52 +0300

    Convert token mixin to custom field

--- models/fields.py
+++ models/fields.py
@@ -1,5 +1,10 @@
+# Third Party (PyPI) Imports
+from nanoid import generate
+from nanoid.resources import alphabet as DEFAULT_ALPHABET
+
 # Django Imports
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # ZION Shared Library Imports
 from zion.conf import settings
@@ -15,3 +20,20 @@ class TimeZoneField(models.CharField):
         }
         defaults.update(kwargs)
         return super(TimeZoneField, self).__init__(*args, **defaults)
+
+
+class TokenField(models.CharField):
+    def __init__(self, *args, **kwargs):
+        self.alphabet = kwargs.pop("alphabet", DEFAULT_ALPHABET)
+        defaults = {
+            "max_length": 32,
+            "unique": True,
+            "editable": False,
+            "verbose_name": _("Token"),
+        }
+        defaults["default"] = self._generate_token
+        defaults.update(kwargs)
+        return super(TokenField, self).__init__(*args, **defaults)
+
+    def _generate_token(self) -> str:
+        return generate(alphabet=self.alphabet, size=self.max_length)
--- models/mixins.py
+++ models/mixins.py
@@ -1,6 +1,3 @@
-# Third Party (PyPI) Imports
-from nanoid import generate
-
 # Django Imports
 from django.db import models
 from django.utils.translation import gettext_lazy as _
@@ -16,41 +13,3 @@ class CreateModifyMixin(models.Model):
 
     class Meta:
         abstract = True
-
-
-class TokenMixin(models.Model):
-    """
-    A mixin that provides a ``token`` field.
-
-    Usage:
-        class MyModel(TokenMixin, token_size=32, editable=False):
-            pass
-    """
-
-    TOKEN_SIZE: int
-    TOKEN_EDITABLE: bool
-
-    class Meta:
-        abstract = True
-
-    def __init_subclass__(cls, **kwargs) -> None:
-        cls.TOKEN_SIZE = kwargs.pop("token_size", 32)
-        cls.TOKEN_EDITABLE = kwargs.pop("token_editable", False)
-
-        super().__init_subclass__(**kwargs)
-
-        cls.token = models.CharField(
-            _("Token"),
-            max_length=cls.TOKEN_SIZE,
-            unique=True,
-            default=cls._generate_token,
-            editable=cls.TOKEN_EDITABLE,
-        )
-
-    @classmethod
-    def _generate_token(cls) -> str:
-        return generate(size=cls.TOKEN_SIZE)
-
-    def generate_token(self) -> None:
-        self.token = self._generate_token()
-        self.save()
