4100435b7afa2db2e5809d3532d15b45654a6406
goztrk <me@gokhan.org.tr>
2023-11-25 12:51:00 +0300

    Add EmailBackend

---
+++ apps/accounts/backends.py
@@ -0,0 +1,16 @@
+# Django Imports
+from django.contrib.auth.backends import ModelBackend
+
+
+class EmailBackend(ModelBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        if username is None:
+            username = kwargs.get("username")
+        try:
+            user = self.user_model.objects.get(email=username)
+        except self.user_model.DoesNotExist:
+            return None
+        else:
+            if user.check_password(password):
+                return user
+        return None
