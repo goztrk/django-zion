b39d3de3ade9722783c6450230f6f20716cc2ddc
goztrk <me@gokhan.org.tr>
2023-11-25 11:55:35 +0300

    Fix mixin name

--- apps/accounts/models/email.py
+++ apps/accounts/models/email.py
@@ -10,7 +10,7 @@ from django.utils.translation import gettext_lazy as _
 
 # ZION Shared Library Imports
 from zion.apps.accounts.conf import settings
-from zion.models.mixins import CreateUpdateMixin
+from zion.models.mixins import CreateModifyMixin
 
 
 class EmailManager(models.Manager):
@@ -19,7 +19,7 @@ class EmailManager(models.Manager):
         return self.get(user=user, is_primary=True)
 
 
-class EmailAddress(CreateUpdateMixin, models.Model):
+class EmailAddress(CreateModifyMixin, models.Model):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         verbose_name=_("User"),
@@ -60,3 +60,21 @@ class EmailAddress(CreateUpdateMixin, models.Model):
 
         return Ok(True)
 
+
+class EmailConfirmation(CreateModifyMixin, models.Model):
+    """Email confirmation model.
+    In progress
+    """
+
+    email_address = models.ForeignKey(
+        EmailAddress, on_delete=models.CASCADE, verbose_name=_("Email Address")
+    )
+    token = models.CharField(max_length=64, unique=True, verbose_name=_("Token"))
+
+    class Meta:
+        verbose_name = _("Email Confirmation")
+        verbose_name_plural = _("Email Confirmations")
+        ordering = ["-created"]
+
+    def __str__(self):
+        return f"Confirmation for {self.email_address}"
--- models/mixins.py
+++ models/mixins.py
@@ -1,14 +1,15 @@
 # Django Imports
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
-class CreateUpdateMixin(models.Model):
+class CreateModifyMixin(models.Model):
     """
     A mixin that provides self-updating ``created`` and ``modified`` fields.
     """
 
-    created = models.DateTimeField(auto_now_add=True)
-    modified = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(_("Created At"), auto_now_add=True)
+    modified = models.DateTimeField(_("Modified At"), auto_now=True)
 
     class Meta:
         abstract = True
