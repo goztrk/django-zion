1c69c927e9a7daab6a0b07e3ed02d960e233bef0
goztrk <me@gokhan.org.tr>
2023-12-02 14:47:15 +0300

    Fix HttpErrorResponseMiddleware
    It appears that `__call__` method is required but I did not see any information on that matter in the docs.

--- middlewares/http_error_response.py
+++ middlewares/http_error_response.py
@@ -10,6 +10,9 @@ class HttpErrorResponseMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
+    def __call__(self, request, *args, **kwargs):
+        return self.get_response(request)
+
     def process_exception(self, request, exception):
         if isinstance(exception, HttpResponseError):
             return exception.response
